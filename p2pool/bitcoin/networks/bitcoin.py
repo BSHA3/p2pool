76621f068e5e58bae6fce32829eb7b98a13f837d
Jon Layton <me@jonl.io>
2018-11-06 18:04:47 -0600

    Update network info for bsha3 (in bitcoin files)

--- p2pool/bitcoin/networks/bitcoin.py
+++ p2pool/bitcoin/networks/bitcoin.py
@@ -7,22 +7,22 @@ from .. import data, helper
 from p2pool.util import pack
 
 
-P2P_PREFIX = 'f9beb4d9'.decode('hex')
-P2P_PORT = 8333
-ADDRESS_VERSION = 0
-RPC_PORT = 8332
+P2P_PREFIX = 'fabfb5da'.decode('hex')
+P2P_PORT = 8335
+ADDRESS_VERSION = 83
+RPC_PORT = 8334
 RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            (yield helper.check_genesis_block(bitcoind, '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')) and
+            (yield helper.check_genesis_block(bitcoind, '0000000090692ea88f3c87dc1cd28e3972c6b86d6f2e7c8fd8d267e88c4d65f1')) and
             (yield bitcoind.rpc_getblockchaininfo())['chain'] != 'test'
         ))
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//210000
 POW_FUNC = data.hash256
 BLOCK_PERIOD = 600 # s
-SYMBOL = 'BTC'
-CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Bitcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Bitcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.bitcoin'), 'bitcoin.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'https://blockchain.info/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'https://blockchain.info/address/'
-TX_EXPLORER_URL_PREFIX = 'https://blockchain.info/tx/'
+SYMBOL = 'BSHA3'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'BSHA3') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/BSHA3/') if platform.system() == 'Darwin' else os.path.expanduser('~/.bsha3'), 'bitcoin.conf')
+BLOCK_EXPLORER_URL_PREFIX = 'http://explorer.bsha3.com/block-height/'
+ADDRESS_EXPLORER_URL_PREFIX = 'http://explorer.bsha3.com/address/'
+TX_EXPLORER_URL_PREFIX = 'http://explorer.bsha3.com/tx/'
 SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
 DUMB_SCRYPT_DIFF = 1
 DUST_THRESHOLD = 0.001e8
